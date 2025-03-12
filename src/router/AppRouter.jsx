c728cf197e1202d76544b65825caea57d65ba9f3
graciamm16 <graciamartin007@gmail.com>
2025-03-12 13:47:17 +0100

    Nuevo proyecto

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../ui";
+import { DcPage, MarvelPage } from "../heroes";
+import { LoginPage } from "../auth";
+
+export const AppRouter = () => {
+    return(
+        <>
+            <Navbar/>
+
+            <Routes>
+                <Route path="/marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="/" element={<Navigate to="marvel"/>} />
+            </Routes>
+        </>
+    )
+}
